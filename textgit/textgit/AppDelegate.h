288723f874684af3f81e59e59299f9d80f6ff7ba
sidiaoling <sidiaoling@qq.com>
2015-08-25 17:46:04 +0800

    first git

---
+++ textgit/textgit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  textgit
+//
+//  Created by apple on 15/8/25.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
